0b7f5bb25a2c7d085fe97048a3ff63f34461128c
Becca Manning <rebm01@hotmail.co.uk>
2020-04-17 11:13:43 +0100

    Install and use Open Sans font

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink, Route, Redirect } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
+import Fade from 'react-reveal/Fade';
 import Home from '../Pages/Home';
 import Therapies from '../Pages/Therapies';
 import Pricing from '../Pages/Pricing';
@@ -20,11 +21,11 @@ const App = () => {
                 {isSmallScreen ? <MobileNav/> : <DesktopNav/>}
             </div>
             <div className="content">
-                <Route exact path="/" render={() => <Home/>}/>
-                <Route path="/therapies" render={() => <Therapies/>}/>
-                <Route path="/pricing" render={() => <Pricing/>}/>
-                <Route path="/blog" render={() => <Blog/>}/>
-                <Route path="/contact" render={() => <Contact/>}/>
+                <Route exact path="/" render={() => <Fade><Home/></Fade>}/>
+                <Route path="/therapies" render={() => <Fade><Therapies/></Fade>}/>
+                <Route path="/pricing" render={() => <Fade><Pricing/></Fade>}/>
+                <Route path="/blog" render={() => <Fade><Blog/></Fade>}/>
+                <Route path="/contact" render={() => <Fade><Contact/></Fade>}/>
                 <Route path="/privacy" render={() => <h4>privacy</h4>}/>
                 <Route path="/faq" render={() => <h4>faq</h4>}/>
                 <Route path="/qualifications" render={() => <h4>qualifications</h4>}/>
