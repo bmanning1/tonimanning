ac53007617f513ffbd6135d09a5501f40906a091
Becca Manning <rebm01@hotmail.co.uk>
2020-11-17 20:08:18 +0000

    Updates

---
+++ precache-manifest.43a9b16397599697cd19982ebad9debf.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94840f4e64a8d4d503315867b804c5b0",
+    "url": "/tonimanning/index.html"
+  },
+  {
+    "revision": "215810dc9547666d4aee",
+    "url": "/tonimanning/static/css/2.53b9c325.chunk.css"
+  },
+  {
+    "revision": "47843f6c9006dfde9677",
+    "url": "/tonimanning/static/css/main.2ad6667e.chunk.css"
+  },
+  {
+    "revision": "215810dc9547666d4aee",
+    "url": "/tonimanning/static/js/2.ce1726b1.chunk.js"
+  },
+  {
+    "revision": "26d02b39008a1b72a5d86e12a3cceb69",
+    "url": "/tonimanning/static/js/2.ce1726b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47843f6c9006dfde9677",
+    "url": "/tonimanning/static/js/main.1bc8c109.chunk.js"
+  },
+  {
+    "revision": "982a446ff381dfc65635",
+    "url": "/tonimanning/static/js/runtime-main.e184085b.js"
+  },
+  {
+    "revision": "bcf18fb479a12ae501158523bbaf7811",
+    "url": "/tonimanning/static/media/Background.bcf18fb4.jpg"
+  },
+  {
+    "revision": "7fca8cdf830c179a33d03bd35dfdb7da",
+    "url": "/tonimanning/static/media/Bespoke.7fca8cdf.jpg"
+  },
+  {
+    "revision": "853ca5d47699bb3beb2c5125c5d4200d",
+    "url": "/tonimanning/static/media/DeepRelaxation.853ca5d4.jpg"
+  },
+  {
+    "revision": "f7485764d3004102f1224a816bac5836",
+    "url": "/tonimanning/static/media/Facial.f7485764.jpg"
+  },
+  {
+    "revision": "e0009a3d2fa332880f5208df6fb5a76c",
+    "url": "/tonimanning/static/media/GiftVouchers.e0009a3d.jpg"
+  },
+  {
+    "revision": "926458b7df3d0661921159cf8be6855b",
+    "url": "/tonimanning/static/media/HotStone.926458b7.jpg"
+  },
+  {
+    "revision": "e5924b6a2ce3c95a9522c5a0e82e670a",
+    "url": "/tonimanning/static/media/IndianHead.e5924b6a.jpg"
+  },
+  {
+    "revision": "4540d345bc2b094826784422374a3aee",
+    "url": "/tonimanning/static/media/Logo.4540d345.png"
+  },
+  {
+    "revision": "1a1dfb1e5fcfcae198f23edf751e41e8",
+    "url": "/tonimanning/static/media/Online.1a1dfb1e.jpg"
+  },
+  {
+    "revision": "e1a5bac37cfbe7cd315d7b318073a419",
+    "url": "/tonimanning/static/media/OpenSans-Light.e1a5bac3.woff"
+  },
+  {
+    "revision": "617203e30b66cdda914bfeb0e873795e",
+    "url": "/tonimanning/static/media/Reflexology.617203e3.jpg"
+  },
+  {
+    "revision": "f13ebbe185b2f03b20127cd11495fe2c",
+    "url": "/tonimanning/static/media/Sports.f13ebbe1.jpg"
+  },
+  {
+    "revision": "4e9fe787c2144a8abf859fcd9a9866ec",
+    "url": "/tonimanning/static/media/Toni.4e9fe787.jpg"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/tonimanning/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/tonimanning/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/tonimanning/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/tonimanning/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/tonimanning/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/tonimanning/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/tonimanning/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/tonimanning/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/tonimanning/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/tonimanning/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/tonimanning/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/tonimanning/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/tonimanning/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/tonimanning/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/tonimanning/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/tonimanning/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
