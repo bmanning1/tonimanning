fda8c42299b2234a6cb73c8cf0681e8265634a5e
Becca Manning <rebm01@hotmail.co.uk>
2020-11-26 15:08:22 +0000

    Fix max width lost in merge

--- src/containers/App/styles.js
+++ src/containers/App/styles.js
@@ -2,6 +2,7 @@ import { Box, withStyles } from '@material-ui/core';
 
 export const StyledBox = withStyles(() => ({
     root: {
+        maxWidth: '48rem',
         overflow: ({ overflow }) => overflow,
         margin: 'auto',
         padding: '0 1.8rem'
